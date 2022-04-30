3a38ceb3a787b9b296b8a9d387f2fe5b8ee2bc72
Edwin Leal <edwinleal019@gmail.com>
2022-04-30 21:00:49 +0200

    feat(views): designs and setouts views done

--- src/views/Designs/index.tsx
+++ src/views/Designs/index.tsx
@@ -1,12 +1,58 @@
-import { FunctionComponent } from "react";
+import { Table } from "components";
+import { formatDate, splitElemets } from "helpers";
+import { FunctionComponent, useEffect, useState } from "react";
+import { get_designs, get_users } from "services/getters";
+import { IDesign, IUser } from "types";
 
 interface DesignsProps {}
 
 const Designs: FunctionComponent<DesignsProps> = () => {
+  const [designs, setDesigns] = useState<IDesign[][]>([]);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  const COLUMNS = ["name", "courses", "wales", "last updated", "by"];
+
+  useEffect(() => {
+    const getData = async () => {
+      const [userData, userError] = await get_users();
+      const [designsData, designError] = await get_designs();
+      if (!designError) {
+        const designs: IDesign[][] = splitElemets(designsData, 5);
+        setDesigns(designs);
+      }
+
+      if (!userError) {
+        setUsers(userData);
+      }
+    };
+    getData();
+  }, []);
+
+  const filterUser = (id: number): string | undefined => {
+    const user = users.find((user) => user.id === id);
+    return user?.name;
+  };
+
   return (
-    <>
-      <h1>DESIGNS</h1>
-    </>
+    <div className="setouts-container">
+      {designs[0] && (
+        <Table columns={COLUMNS}>
+          {designs[0].map((item: IDesign) => (
+            <tr key={item.id} className="t-row">
+              <td>{item.name}</td>
+              <td>{item.courses}</td>
+              <td>{item.wales}</td>
+              <td>{formatDate(item.updated)}</td>
+              <td>
+                {filterUser
+                  ? filterUser(item.user_id_last_update)
+                  : "User not found"}
+              </td>
+            </tr>
+          ))}
+        </Table>
+      )}
+    </div>
   );
 };
 
--- src/views/Setouts/index.tsx
+++ src/views/Setouts/index.tsx
@@ -1,9 +1,50 @@
-import { FunctionComponent } from "react";
+import { Table } from "components";
+import { FunctionComponent, useEffect, useState } from "react";
+import { get_setouts } from "services/getters";
+import "./Setouts.scss";
+import { formatDate, splitElemets } from "helpers";
+import { ISetout } from "types";
 
 interface SetoutsProps {}
 
 const Setouts: FunctionComponent<SetoutsProps> = () => {
-  return <h1>Setouts</h1>;
+  const [setouts, setSetouts] = useState<ISetout[][]>([]);
+  const COLUMNS = [
+    "name",
+    "machine name",
+    "machine width",
+    "courses",
+    "last updated",
+  ];
+  useEffect(() => {
+    const getSetouts = async () => {
+      const [setoutsData, setoutsError] = await get_setouts();
+      if (!setoutsError) {
+        const setouts: ISetout[][] = splitElemets(setoutsData, 5);
+        setSetouts(setouts);
+      }
+    };
+
+    getSetouts();
+  }, []);
+
+  return (
+    <div className="setouts-container">
+      {setouts[0] && (
+        <Table columns={COLUMNS}>
+          {setouts[0].map((item: any) => (
+            <tr key={item.id} className="t-row">
+              <td>{item.name}</td>
+              <td>{item.machine_name}</td>
+              <td>{item.machine_width}</td>
+              <td>{item.courses}</td>
+              <td>{formatDate(item.updated)}</td>
+            </tr>
+          ))}
+        </Table>
+      )}
+    </div>
+  );
 };
 
 export default Setouts;
